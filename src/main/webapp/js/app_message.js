02afd894265a3443b40a4ff80cf1f640463f7974
shiliting <861819089@qq.com>
2016-12-03 11:58:37 +0800

    优化4

--- src/main/webapp/js/app_message.js
+++ src/main/webapp/js/app_message.js
@@ -11,7 +11,8 @@ messageApp.buildList=function(list){
     for(var i=0;i<list.length;i++){
         var message=list[i];
         var html='<a href="/app/showMessage?customerId='+customerId+'&id='+message.id+'" class="weui_media_box weui_media_appmsg"> ' +
-            '<div class="weui_media_hd"> <img class="weui_media_appmsg_thumb" src="'+message.pictureUrl+'" alt=""/> ' +
+            '<div class="weui_media_hd"> ' +
+            '<img style="margin-top: 15px" class="weui_media_appmsg_thumb" src="'+message.pictureUrl+'" alt=""/> ' +
             '</div> <div class="weui_media_bd"> ' +
             '<h4 class="weui_media_title">'+message.title+'</h4> ' +
             '<p class="weui_media_desc">'+message.summary+'</p>' +
@@ -49,13 +50,15 @@ messageApp.loadList=function(){
         data: {lastId:lastId,customerId:customerId},
         success: function (result) {
             var list=result.list;
-            if(list.length<=0){
-                isEmpty=true;
-                $("#lookMore").hide();
-            }else {
+            if(list.length>0){
                 lastId=list[list.length-1].id;
                 messageApp.buildList(list);
             }
+            if(list.length<20){
+                isEmpty=true;
+                $("#lookMore").hide();
+            }
+
         }
     });
 };
